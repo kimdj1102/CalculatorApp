72c7b89a777ec2dd156f69cedfcc67674d511acc
kimdj1102 <djkim1102@naver.com>
2024-11-22 15:31:29 +0900

    Initial Commit

---
+++ [1]Calculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  [1]Calculator
+//
+//  Created by 김동준 on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ [1]CalculatorTests/_1_CalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  _1_CalculatorTests.swift
+//  [1]CalculatorTests
+//
+//  Created by 김동준 on 11/22/24.
+//
+
+import Testing
+@testable import _1_Calculator
+
+struct _1_CalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
