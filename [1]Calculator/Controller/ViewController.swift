3bba6e1c20d855284ae8c10e0b4529cab0dd1918
kimdj1102 <djkim1102@naver.com>
2024-12-04 22:03:19 +0900

    [refactor] 버튼을 통한 label 숫자 입력 관련 기능에 관련된 내용  mvc 패턴 기반 리펙토링

---
+++ [1]Calculator/Controller/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  [1]Calculator
+//
+//  Created by 김동준 on 11/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var numberLabel: UILabel!
+
+    var textManager = TextChangeManager()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        setup()
+        
+        numberLabel.text = "0"
+    }
+
+    func setup() {
+        numberLabel.lineBreakMode = .byTruncatingHead
+    }
+    
+    @IBAction func numberBtnPressed(_ sender: UIButton) {
+        let number = sender.tag - 10
+        guard let label = numberLabel.text else { return }
+        
+        numberLabel.text = textManager.addText(label, number)
+    }
+    
+    @IBAction func deleteBtnPressed(_ sender: UIButton) {
+        numberLabel.text?.removeLast()
+    }
+    
+}
---
+++ [1]Calculator/Model/TextChangeManager.swift
@@ -0,0 +1,54 @@
+//
+//  model.swift
+//  [1]Calculator
+//
+//  Created by 김동준 on 12/3/24.
+//
+
+import UIKit
+
+struct TextChangeManager {
+    var totalResult = 0
+    
+    mutating func addText(_ currentText: String, _ num: Int) -> String {
+        print(totalResult)
+        
+        if totalResult == 0 && num == 0 {
+            return "0"
+        }
+        
+        let commaRemoved = removeComma(currentText)
+        let resultText = commaRemoved + String(num)
+        
+        if let result = Int(resultText) {
+            totalResult = Int(result)
+            
+            return addComma(resultText)
+        }
+        else {
+            return currentText
+        }
+    }
+    
+    func addComma(_ text: String) -> String {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .decimal
+        
+        if let num = Int(text),
+           let formattedString = formatter.string(from: NSNumber(value: num)) {
+            return formattedString
+        }
+        else {
+            return text
+        }
+    }
+    
+    func removeComma(_ text: String) -> String {
+        let currentText = text.replacingOccurrences(of: "[.,]", with: "", options: .regularExpression)
+        
+        return currentText
+    }
+}
+
+
+
